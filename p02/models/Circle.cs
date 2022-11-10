7a981b0a33c6fc1e50fad43dd51babb890dd217e
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-10 22:23:32 +0000

    Create Circle class

--- p02/models/Circle.cs
+++ p02/models/Circle.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p02.models
-{
-    class Circle
-    {
+namespace p02.models {
+    class Circle {
+        private Point2D center;
+        private double r;
+
+        public Circle() {
+        
+        }
+
+        public Circle(Point2D p, double r) {
+            center = p;
+            this.r = r;
+        }
+
+        public void SetCenter(Point2D p) {
+            center = p;
+        }
+
+        public Point2D GetCenter() {
+            return center;
+        }
+
+        public void SetR(double r) {
+            this.r = r;
+        }
+
+        public double GetR(){
+            return r;
+        }
+
     }
 }
