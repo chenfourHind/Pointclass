2c23a0c9c282916be7fa0f391923400918e75f9a
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-10 21:27:46 +0000

    Create Point class

---
+++ p02/models/Point3D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02.models {
+    class Point3D {
+        private double x;
+        private double y;
+
+        public Point3D() {
+
+        }
+
+        public Point3D(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetX(double x) {
+            this.x = x;
+        }
+
+        public double GetX() {
+            return x;
+        }
+
+        public void SetY(double y) {
+            this.y = y;
+        }
+
+        public double GetY() {
+            return y;
+        }
+
+    }
+}
